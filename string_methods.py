7da1266194fbcce93e2792144dacf76f4bc99caa
Kepabonsa <contactkepabonsa@gmail.com>
2025-09-28 18:45:52 -0500

    avances python 9/28/2025

---
+++ string_methods.py
@@ -0,0 +1,17 @@
+
+# name = input("Enter your full name ")
+phone_number = input("Enter your full phone number")
+# result = len(name) #Largo del string
+# result = name.find("v") #Encontrar letra
+# result = name.rfind("b") #Encontrar letra al reves
+# name = name.capitalize() #Poner la primera letra mayuscula
+# name = name.upper() #Poner todas las letras en mayuscula 
+# name = name.lower() #Poner todas las letras en minuscula
+# result = name.isdigit() #Verifica si hay solamente digitos en el string y devuelve un boolean T/F
+# result = name.isalpha() #Verifica si  solamente hay caracteres del alfabeto en el string y devuelve un boolean T/f
+# result = phone_number.count("-") #Cuenta cuantos caracteres hay en el string según el solicitado
+# phone_number = phone_number.replace("-", " ") #Remplaza X caracter con un caracter Y
+# print(help(str)) #Para buscar ayuda sobre metodos de strigs
+
+print(phone_number)
+
