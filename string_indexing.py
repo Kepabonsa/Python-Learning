9708dbf7f915a08c599fbc1f7b70eca220d78cfd
Kepabonsa <contactkepabonsa@gmail.com>
2025-09-30 18:59:15 -0500

    quick lesson 9/30/25

---
+++ string_indexing.py
@@ -0,0 +1,24 @@
+# indexing = accessing elements of a sequence usin [] (indexing operator)
+# [start : end : ]
+
+credit_number = "1234-5678-9012-3456"
+
+# Start and End
+
+# print(credit_number[0])
+# print(credit_number[0:4]) 
+# print(credit_number[5:9])
+# print(credit__number[5:])
+# print(credit_number[-1])
+
+# Step
+# print(credit_number[::2])
+
+# Program Exercise 1
+
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+# Program Exercise 2
+credit_number =credit_number[::-1]
+print(credit_number)
